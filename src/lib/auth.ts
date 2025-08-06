45f3037c9873b090f4f572f9f03ba4202974eee4
Nat1anWasTaken <nathan@nat1an.xyz>
2025-08-06 10:08:25 +0800

    style(*): run formatter

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,12 @@
-import { betterAuth } from 'better-auth';
-import { PrismaClient } from '@prisma/client';
-import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { betterAuth } from "better-auth";
+import { PrismaClient } from "@prisma/client";
+import { prismaAdapter } from "better-auth/adapters/prisma";
 
 const prisma = new PrismaClient();
 
 export const auth = betterAuth({
-	database: prismaAdapter(prisma, { provider: 'mongodb' }),
-	emailAndPassword: {
-		enabled: true
-	}
+    database: prismaAdapter(prisma, { provider: "mongodb" }),
+    emailAndPassword: {
+        enabled: true
+    }
 });
--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,13 +1,13 @@
 <script>
-	import ModeSwitch from './mode-switch.svelte';
+    import ModeSwitch from "./mode-switch.svelte";
 </script>
 
 <div class="flex h-16 flex-row items-center justify-between bg-accent px-4">
-	<div class="flex flex-row items-center gap-2">
-		<h1 class="text-lg font-bold">
-			<a href="/">When2Meet</a>
-		</h1>
-		<a href="/">About</a>
-	</div>
-	<ModeSwitch />
+    <div class="flex flex-row items-center gap-2">
+        <h1 class="text-lg font-bold">
+            <a href="/">When2Meet</a>
+        </h1>
+        <a href="/">About</a>
+    </div>
+    <ModeSwitch />
 </div>
