bab991bac72a54004d98e3b9bb50082f40961e87
Nat1anWasTaken <nathan@nat1an.xyz>
2025-11-19 13:34:57 +0800

    chore(package.json, src/app.d.ts): update scripts and add env module types

--- src/app.d.ts
+++ src/app.d.ts
@@ -12,4 +12,12 @@ declare global {
     }
 }
 
+declare module "$env/static/private" {
+    export const DATABASE_URL: string;
+    export const GITHUB_CLIENT_ID: string;
+    export const GITHUB_CLIENT_SECRET: string;
+    export const GOOGLE_CLIENT_ID: string;
+    export const GOOGLE_CLIENT_SECRET: string;
+}
+
 export {};
